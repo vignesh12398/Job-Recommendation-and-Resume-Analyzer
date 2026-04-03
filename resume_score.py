dab1d56e00cf5cbfbeba3f22da493c38619a8b14
vignesh12398 <vigneshbalajivigneshbalaji185@gmail.com>
2026-04-03 17:25:17 +0530

    Update resume_score.py

--- resume_score.py
+++ resume_score.py
@@ -15,7 +15,7 @@ def predict_score(resume_text):
     X = df['text']
     y = df['Score']
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y
+        X, y, test_size=0.2, random_state=42
     )
     X_train = v.fit_transform(X_train)
     X_test = v.transform(X_test)
